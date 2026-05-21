be1562d08852b01a7c7442d0d2a07144b5d5a154
Gary Peterson <petergo@mail.uc.edu>
2026-05-21 13:00:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 function showResponse(type){
 
-const box = document.getElementById("responseBox");
+const box =
+document.getElementById("responseBox");
 
 if(type === "engine"){
 
 box.innerHTML = `
-<h2>CHECK ENGINE</h2>
+
+<h2 style="color:#ffcc00;">
+CHECK ENGINE LIGHT
+</h2>
+
 <p>
-Usually safe to drive briefly if steady yellow.
-Possible sensor, ignition, or emissions issue.
+
+Usually safe for short driving
+if steady yellow.
+
+Possible causes:
+<br><br>
+
+• loose gas cap
+<br>
+• ignition issue
+<br>
+• emissions issue
+<br>
+• faulty sensor
+
+<br><br>
+
 Schedule diagnostic service soon.
+
 </p>
+
 `;
 
 }
@@ -18,12 +40,26 @@ Schedule diagnostic service soon.
 if(type === "oil"){
 
 box.innerHTML = `
-<h2>OIL PRESSURE WARNING</h2>
+
+<h2 style="color:#ff3300;">
+OIL PRESSURE WARNING
+</h2>
+
 <p>
+
 PULL OVER SAFELY.
-Low oil pressure can destroy the engine quickly.
-Turn vehicle off and call for assistance.
+<br><br>
+
+Low oil pressure may cause
+catastrophic engine damage.
+
+<br><br>
+
+Turn engine OFF and
+call for assistance.
+
 </p>
+
 `;
 
 }
@@ -31,12 +67,25 @@ Turn vehicle off and call for assistance.
 if(type === "battery"){
 
 box.innerHTML = `
-<h2>BATTERY WARNING</h2>
+
+<h2 style="color:#ff3300;">
+BATTERY WARNING
+</h2>
+
 <p>
+
 Charging system problem detected.
-Vehicle may shut off soon.
-Limit driving and head to service immediately.
+
+<br><br>
+
+Vehicle may shut off unexpectedly.
+
+<br><br>
+
+Drive directly to service.
+
 </p>
+
 `;
 
 }
@@ -44,12 +93,26 @@ Limit driving and head to service immediately.
 if(type === "temp"){
 
 box.innerHTML = `
-<h2>ENGINE TEMP WARNING</h2>
+
+<h2 style="color:#ff3300;">
+ENGINE TEMP WARNING
+</h2>
+
 <p>
-PULL OVER SAFELY.
-Engine overheating detected.
-Turn vehicle off and allow cooling before continuing.
+
+ENGINE OVERHEATING DETECTED.
+
+<br><br>
+
+Pull over immediately.
+
+<br><br>
+
+Turn vehicle OFF and
+allow engine to cool.
+
 </p>
+
 `;
 
 }
